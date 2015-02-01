a18116499c798f49e966bb1c75be6a1c2ac9b4ef
Jeffery Zhao <zzy8200@gmail.com>
2015-02-01 10:16:09 -0800

    add backup data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Password Manager
 ##Version
-v1.6  
+v1.8  
 ##Specification
 This password manager can generate random strong passwords for users. The password is generated on user's browser and then encrypt using AES256.  
 Then the browser will upload the encrypted password to server so server can never know what the password is. The decrypted process also executed by the browser and the user need a secret key to decrypt the password.  
--- Source/password.php
+++ Source/password.php
@@ -68,10 +68,11 @@ function decrypt($data, $key)
 <?php
 session_start(); 
 if(!isset($_SESSION["loginok"]) || $_SESSION['loginok']!=1) {session_destroy();header("Location: ./");die();}
-require_once("../../functionzzeyu/sqllink.php");
-require_once("../../functionzzeyu/basic.php");
-$link=sqllink();
-if(!$link) die();
+require_once("sqllink.php");
+	$link=sqllink()
+if(!$link) {
+    session_destroy();header("Location: ./");die();
+}
 $usr=$_SESSION['user'];
 $pw=$_SESSION['pwd'];
 if($usr==""||$pw=="")  {session_destroy();header("Location: ./");die();}
@@ -80,14 +81,12 @@ $rett=mysql_query($sql,$link);
 $num= mysql_num_rows($rett);
 if($num==0) {session_destroy();header("Location: ./");die();}
 setcookie("pwdrecord".$usr,$pw, time()+5160000);
-echoheader(2);
 ?>
-<style type="text/css">
-@font-face {
-	font-family: 'passwordshow';
-	src:url('pw.ttf');
-}
-</style>
+<!DOCTYPE HTML>
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<title>ZerUniverse</title>
 <script type="text/javascript" src="setcookies.js"></script>
 <script type="text/javascript">
 var secretkey="";
@@ -95,13 +94,12 @@ function quitpwd(usr)
 {
 	if(usr=="") {delpwdcookies(); window.location.href="./logout.php";} else {delpwdcookies();document.cookie="pwdrecord"+usr+"=;expires="+(new Date(0)).toGMTString(); window.location.href="./logout.php";}
 }
-
 function showtable()
 {
-	if($("#secretkey").val()=='') {alert("不能为空！");return ;}
+	if($("#secretkey").val()=='') {alert("can't be empty！");return ;}
 	$("#secretkey").attr("readonly",true);
 	$("#keybtn").attr("onClick","changesk();");
-	$("#keybtn").attr("value","重新输入私钥");
+	$("#keybtn").attr("value","Input Secret Key");
 	secretkey=$("#secretkey").val();
 	$("#pwdtable").show();
 	setpwdcookies(secretkey);
@@ -114,46 +112,65 @@ function changesk()
 	
 }
 </script>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="aes.js"></script>
-<script type="text/javascript" src="sha512.js"></script>
+
 
 
 <script type="text/javascript" src="password.js"></script>
-<div class="container theme-showcase">
-      <div class="page-header">
-        <h1>密码管理器</h1>
-	  </div>
-      <span style="display:none; font-family:passwordshow">abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ</span>
-	<div>请输入私钥：<form><input id="secretkey" type="password" />&nbsp;<input type="button" id="keybtn" class="btn btn-sm btn-success" onClick="showtable();" value="确认"/>&nbsp;|&nbsp;<input class="btn btn-sm btn-default" type="button" onClick="window.location.href='backup.php'" value="备份数据"/>&nbsp;|&nbsp;<input class="btn btn-sm btn-info" type="button" onClick="quitpwd('');" value="安全退出"/>&nbsp;<input class="btn btn-sm btn-danger" type="button" onClick="quitpwd('<?php echo $usr;?>');" value="退出并取消信任此设备"/></form></div>
-    <hr />
+</head>
+<body>
+<!-- Begin Wrapper -->
+<div style="padding:50px;">
+	
+	<!-- Begin Content -->
+	<div>
+	<h1 class="title">Password Manager</h1>
+	<div class="line"></div>
+	<div>Input secret key：<form><input id="secretkey" type="password" /><input type="button" id="keybtn" onClick="showtable();" value="OK"/>&nbsp;|&nbsp;<input class="btn btn-sm btn-default" type="button" onClick="window.location.href='backup.php'" value="BackupData"/>&nbsp;|&nbsp;<input type="button" onClick="quitpwd('');" value="logout"/><input type="button" onClick="quitpwd('<?php echo $usr;?>');" value="Logout and forget this device"/></div>
+    <div class="line"></div>
     <div id="pwdtable" style="display:none">
-    <table class="table">
-    <tr><th>密码名称</th><th>密码</th><th>更新此密码</th><th>删除此密码</th></tr>
+    <table>
+    <tr><th>Title</th><th>Password</th><th>Generate a new</th><th>Delete</th></tr>
     <?php
 		$sql="SELECT * FROM `password` WHERE `usr`='$usr'";
 		$rett=mysql_query($sql, $link);
-		while ($i = mysql_fetch_array($rett)){ //返回查询结果
+		while ($i = mysql_fetch_array($rett)){ 
 			$index=$i['index'];
 			$name=$i['name'];
 			$kss=decrypt($i['pwd'],$i['pubkey']);
-			echo "<tr><td>".$name.'</td><td><span id="'.$index.'"><a href="javascript: clicktoshow(\''.$kss.'\',\''.$index.'\')">点击查看密码</a></span></td><td><a href="javascript: refreshpw(\''.$index.'\',\''.$name.'\',\''.$kss.'\')">点击更新</a></td><td><a href="javascript: delepw(\''.$index.'\',\''.$name.'\')">点击删除</a></td></tr>';
+			echo "<tr><td>".$name.'</td><td><span id="'.$index.'"><a href="javascript: clicktoshow(\''.$kss.'\',\''.$index.'\')">Click to see</a></span></td><td><a href="javascript: refreshpw(\''.$index.'\',\''.$name.'\',\''.$kss.'\')">Click to generate a new password</a></td><td><a href="javascript: delepw(\''.$index.'\',\''.$name.'\')">click to delete</a></td></tr>';
 		}
 mysql_free_result($rett);
-// 关闭连接
 mysql_close($link);
-?></table> 
-	<hr />
-    <div class="jumbotron">
-      	<p><h2>新增一个项目</h2></p>
-        <form>
-             <p>输入项目名称：<input id="newiteminput" type="text" /></p>
-             <p>输入对应密码：<input id="newiteminputpw" type="text" placeholder="留空则随机产生"/></p>
-             <p><input type="button" class="btn btn-sm btn-primary" id="newbtn"  value="增加"/></p>
-        </form>
-      </div>
+?></table> <div class="line"></div><div class="toggle">
+        <h2 class="trigger">+ Add a new password</h2>
+        <div class="togglebox">
+          <div>
+            <form>
+             Title:<input id="newiteminput" type="text" />
+             <input type="button" id="newbtn"  value="Add"/>
+             </form>
+             </div>
+             </div></div></div>
+    <!-- Begin Footer -->
+    <div id="footer">
+  	&copy;Jeffery Zhao; 2014. Alpha<br /><br />
+    </div>
+    <!-- End Footer -->
+    
+    
+	</div>
+	<!-- End Content -->
 
 </div>
-</div>
+<!-- End Wrapper -->
+<div class="clear"></div>
+
+
+</body>
+</html>
 <script type="text/javascript">
 $(document).ready(function(){ 
 	var gett=getpwdcookies();
@@ -162,53 +179,48 @@ $(document).ready(function(){
 	$("#secretkey").attr("readonly",true);
 	$("#secretkey").attr("value",gett);
 	$("#keybtn").attr("onClick","changesk();");
-	$("#keybtn").attr("value","重新输入私钥");
+	$("#keybtn").attr("value","Reinput secret key");
 	secretkey=gett;
 	$("#pwdtable").show();
 }
 $("#newbtn").click(function(){ 
 	var newpwd;
 	var sk=secretkey;
-	if($("#newiteminput").val()=="") {alert("项目名不能为空！"); return;}
+	if($("#newiteminput").val()=="") {alert("can't be empty"); return;}
 	$("#newbtn").attr("disabled",true);
 	$("#newiteminput").attr("readonly",true);
-	$("#newiteminputpw").attr("readonly",true);
-	if($("#newiteminputpw").val()=='') newpwd=getpwd(); else newpwd=$("#newiteminputpw").val();
-	newpwd=encryptchar(newpwd,sk);
+	newpwd=encryptchar(getpwd(),sk);
 	var name=$("#newiteminput").val();
 	$.post("insert.php",{csfds:'sdf', name:name,newpwd:newpwd},function(msg){ 
-    if(msg==1) {alert("新增"+$("#newiteminput").val()+"成功！");setpwdcookies(secretkey);location.reload(true)} else alert("新增"+$("#newiteminput").val()+"失败！");
+    if(msg==1) {alert("add "+$("#newiteminput").val()+"successfully！");setpwdcookies(secretkey);location.reload(true)} else alert("add "+$("#newiteminput").val()+" fail！");
     $("#newiteminput").attr("readonly",false);
-	$("#newbtn").attr("disabled",false);
-	
-	$("#newiteminputpw").attr("readonly",false);});
+	$("#newbtn").attr("disabled",false);});
 	}); 
 }); 
 function clicktoshow(kss,id){ 
 		if(kss=="") return;
         var thekey=decryptchar(kss,secretkey);
-		if(thekey!="") $("#"+id).html('<span style="font-family:passwordshow">'+thekey+'</span>'); else $("#"+id).html("私钥不匹配");
+		if(thekey!="") $("#"+id).html(thekey); else $("#"+id).html("Secret key not matched");
 } 
 function refreshpw(index,name,oldpw){
 	var newpwd;
 	var sk=secretkey;
-	if(confirm("您真的要产生一个"+name+"的新密码吗？(旧密码将本地解锁后上传服务器)"))
+	if(confirm("Confirm generate a new password for "+name+"?"))
 	{
 		oldpw=decryptchar(oldpw,sk);
-		if(oldpw=="" && !confirm("您现在的私钥与此密码不匹配，无法解锁密码。若强制更新，服务器将不保存您上次密码。")) return;
+		if(oldpw=="" && !confirm("Secret key not matched, server can't record your old password. Please do this at your own risk")) return;
 		newpwd=encryptchar(getpwd(),secretkey);
 		$.post("change.php",{csfds:'sdf', newpwd:newpwd,name:name,index:index,oldpw:oldpw},function(msg){ 
-         if(msg==1) {alert("产生"+name+"新密码成功！");setpwdcookies(secretkey);location.reload(true)} else alert("产生"+name+"新密码失败！");
+         if(msg==1) {alert("generate "+name+" successfully！");setpwdcookies(secretkey);location.reload(true)} else alert("generate "+name+" failed！");
 		 }); 
 }}
 function delepw(index,name)
 {
-	if(confirm("您真的要删除"+name+"的密码吗？(该操作无法撤销)"))
+	if(confirm("Confirm delete "+name+"?"))
 	{
 		$.post("delete.php",{csfds:'sdf', name:name,index:index},function(msg){ 
-         if(msg==1) {alert("删除"+name+"密码成功！");setpwdcookies(secretkey);location.reload(true)} else alert("删除"+name+"密码失败！");
+         if(msg==1) {alert("Delete"+name+"successfully！");setpwdcookies(secretkey);location.reload(true)} else alert("Delete "+name+" fail！");
 	 }); 
 	 }
 }
 </script>
-<?php echofooter();?>
