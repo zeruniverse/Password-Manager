42a47b5b6954da650bd5de735d2dffd744e50fa9
Jeffery Zhao <zzy8200@gmail.com>
2015-08-09 12:57:50 -0700

    random_login_stamp support

--- src/check.php
+++ src/check.php
@@ -2,6 +2,7 @@
 require_once("function/encryption.php");
 require_once("function/sqllink.php");
 session_start(); 
+if(!isset($_SESSION['random_login_stamp']) || $_SESSION['random_login_stamp']=='') {die("4");}
 $usr=$_POST['user'];
 $pw=$_POST['pwd'];
 if($pw==""||$usr=="") die("0");
@@ -11,7 +12,7 @@ $sql="SELECT * FROM `pwdusrrecord` WHERE `username`= ?";
 $res=sqlexec($sql,array($usr),$link);
 $record= $res->fetch(PDO::FETCH_ASSOC);
 if($record==FALSE) die('0');
-if(strcmp(decrypt($record["password"],$GLOBAL_SALT_3),$pw)!=0)die("1");
+if(strcmp(hash('sha512',(string)decrypt($record["password"],$GLOBAL_SALT_3).(string)$_SESSION['random_login_stamp']),$pw)!=0) die("1");
 $_SESSION['loginok']=1;
 $_SESSION['user']=$usr;
 $_SESSION['userid']=$record['id'];
