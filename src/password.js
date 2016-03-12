5f6bb49f2016f4c0bf2c60906a6c3672943a3884
Jeffery Zhao <zzy8200@gmail.com>
2016-03-12 13:33:00 -0800

    no need to parse the encrypted cipherParams

--- src/password.js
+++ src/password.js
@@ -76,64 +76,21 @@ function get_orig_pwd(key,salt,account_sig,orig_alphabet,temp_pwd)
     return pwd;
 }
 
-function tosend(cipherParams) {
-            // create json object with ciphertext
-            var send="";
-               send+=cipherParams.ciphertext.toString(CryptoJS.enc.Base64)+"-";
-            
-
-            // optionally add iv and salt
-            if (cipherParams.iv) {
-                send+=cipherParams.iv.toString()+"-";
-            }
-            if (cipherParams.salt) {
-                send+=cipherParams.salt.toString()+"-";
-            }
-
-            // stringify json object
-            return send;
-        }
- 
- function getsend(sendstr) {
-            // parse json string
-			var i=sendstr.indexOf("-");
-			var u=sendstr.substring(0,i);
-            // extract ciphertext from json object, and create cipher params object
-            var cipherParams = CryptoJS.lib.CipherParams.create({
-                ciphertext: CryptoJS.enc.Base64.parse(u)
-            });
-			sendstr=sendstr.substring(i+1,sendstr.length);
-            // optionally extract iv and salt
-            if (sendstr!="") {
-				i=sendstr.indexOf("-");
-				u=sendstr.substring(0,i);
-                cipherParams.iv = CryptoJS.enc.Hex.parse(u);
-				sendstr=sendstr.substring(i+1,sendstr.length);
-            }
-            if (sendstr!="") {
-				i=sendstr.indexOf("-");
-				u=sendstr.substring(0,i);
-                cipherParams.salt = CryptoJS.enc.Hex.parse(u);
-				sendstr=sendstr.substring(i+1,sendstr.length);
-            }
-
-            return cipherParams;
-        }
 function encryptchar(encryptch,key){  
     if(encryptchar==""||key==""){  
         alert("ERROR: empty key detected!");  
         return;  
     }
-   var p=tosend(CryptoJS.AES.encrypt(encryptch,key));
+   var p=CryptoJS.AES.encrypt(encryptch,key).toString();
    return p;
 }  
 
-function decryptchar(char,key){  
-    if(char==""||key==""){  
+function decryptchar(echar,key){  
+    if(echar==""||key==""){  
         alert("ERROR: empty key detected!");  
         return;  
     }
-	var p=CryptoJS.AES.decrypt(getsend(char),key).toString(CryptoJS.enc.Utf8);
+	var p=CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(echar,key));
     return p;  
 } 
 
