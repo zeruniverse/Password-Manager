7b5d2733a34ffbb9594e17017034a6cad1dddc23
Jeffery Zhao <zzy8200@gmail.com>
2016-03-14 21:23:15 -0700

    history page

--- src/history.php
+++ src/history.php
@@ -3,7 +3,6 @@ session_start();
 if(!isset($_SESSION["loginok"]) || $_SESSION['loginok']!=1) {session_destroy();header("Location: ./");die();}
 require_once("function/sqllink.php");
 require_once("function/basic.php");
-require_once("function/encryption.php");
 $link=sqllink();
 if(!$link) {session_destroy();header("Location: ./");die();}
 $usr=$_SESSION['user'];
@@ -23,13 +22,38 @@ echoheader();
         <h1>Trusted Devices</h1>
     </div>
     <table class="table">
-    <tr><th>Device ID</th><th>Device Type</th><th>Set Time</th><th>Untrust (Disable PIN)</th></tr>
+    <tr><th>Device Type</th><th>Set Time</th><th>Untrust (Disable PIN)</th></tr>
+    <?php
+        $sql="SELECT * FROM `pin` WHERE `userid`= ?";
+        $res=sqlexec($sql,array($id),$link);
+		while ($i = $res->fetch(PDO::FETCH_ASSOC)){ 
+            $did=$i['device'];
+            $ctime=(int)$i['createtime'];
+            $ua=$i['ua'];
+            echo "<tr><td class='uacell'>".$ua."</td><td class='timestampcell'>".$ctime."</td><td><a href='javascript: unsetpin(\"".$did."\")'>Untrust this device</a></td></tr>";
+		}
+    ?>
     </table>
     <div class="page-header">
         <h1>Login History</h1>
     </div>
+    <p>Red entries indicate password error (i.e. error try)</p>
     <table class="table">
-    <tr><th>Device ID</th><th>Device Type</th><th>Login IP</th><th>Login Time</th><th>Login Result</th></tr>
+    <tr><th>Device Type</th><th>Login IP</th><th>Login Time</th></tr>
+    <?php
+        $sql="SELECT * FROM `history` WHERE `userid`= ?";
+        $res=sqlexec($sql,array($id),$link);
+		while ($i = $res->fetch(PDO::FETCH_ASSOC)){ 
+            $ip=$i['ip'];
+            $ua=$i['ua'];
+            $ctime=(int)$i['time'];
+            if((int)$i['outcome']==0)
+                $color=' style="color:red"';
+            else
+                $color='';
+            echo "<tr".$color."><td class='uacell'>".$ua."</td><td>".$ip."<td class='timestampcell'>".$ctime."</td></tr>";
+		}
+    ?>
     </table>   
 </div>
 <?php echofooter();?>
