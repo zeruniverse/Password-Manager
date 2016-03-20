8dba6ef90f92ff4863f73eb631ede80c65af0c19
Jeffery Zhao <zzy8200@gmail.com>
2016-03-20 14:01:48 -0700

    logout clear all newly added sessions

--- src/logout.php
+++ src/logout.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
- if(isset($_SESSION['loginok'])) unset($_SESSION['loginok']);
- if(isset($_SESSION['user'])) unset($_SESSION['user']);
- if(isset($_SESSION['pwd'])) unset($_SESSION['pwd']);
+if(isset($_SESSION['loginok'])) unset($_SESSION['loginok']);
+if(isset($_SESSION['user'])) unset($_SESSION['user']);
+if(isset($_SESSION['pwd'])) unset($_SESSION['pwd']);
+if(isset($_SESSION['fields'])) unset($_SESSION['fields']);
+if(isset($_SESSION['userid'])) unset($_SESSION['userid']);
+if(isset($_SESSION['random_login_stamp'])) unset($_SESSION['random_login_stamp']);
 header("Location: ./");
 ?>
