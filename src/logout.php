76a8a39a53b6b1e27fd113df863f7ae3fff2d032
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-01-05 09:05:20 +0100

    logout: session cookie secure

--- src/logout.php
+++ src/logout.php
@@ -1,4 +1,5 @@
 <?php
+require_once("function/sqllink.php");
 session_start();
 if(isset($_SESSION['loginok'])) unset($_SESSION['loginok']);
 if(isset($_SESSION['user'])) unset($_SESSION['user']);
@@ -6,11 +7,6 @@ if(isset($_SESSION['pwd'])) unset($_SESSION['pwd']);
 if(isset($_SESSION['fields'])) unset($_SESSION['fields']);
 if(isset($_SESSION['userid'])) unset($_SESSION['userid']);
 if(isset($_SESSION['random_login_stamp'])) unset($_SESSION['random_login_stamp']);
-$params = session_get_cookie_params();
-setcookie(session_name(), '', time() - 42000,
-    $params["path"], $params["domain"],
-    $params["secure"], $params["httponly"]
-);
 session_regenerate_id(true);//as suggested by owasp, change sessionId when changing context
 session_destroy();
 $reason = "";
