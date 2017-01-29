2a03a31a93796b97675aa047fc4ae395d6370121
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-01-29 12:19:55 +0100

    set X-Frame-Options: DENY to prevent clickjacking

--- src/function/basic.php
+++ src/function/basic.php
@@ -9,7 +9,8 @@ echo '<footer class="footer">
 }
 function echoheader()
 {
-echo '<!DOCTYPE html>
+    header('X-Frame-Options: DENY');
+    echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -17,8 +18,8 @@ echo '<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="Password Manager">
     <meta name="author" content="Jeffery">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
@@ -27,7 +28,7 @@ echo '<!DOCTYPE html>
     <!-- Fav and touch icons -->
     <link rel="shortcut icon" href="favicon.ico">
     <script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
 <body style="color:#666666">';
 }
