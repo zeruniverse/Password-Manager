6a4175268e4056d446bf420b0ac7ad7d98403715
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-01-29 12:37:50 +0100

    moved all style attributes to style.css

--- src/password.php
+++ src/password.php
@@ -17,7 +17,7 @@ echoheader();
 <script type="text/javascript" src="js/main.js"></script>
    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
-        <div style="display:none">
+        <div class="hidden">
         <form id="historyform" action="history.php" method="post">
         <input type="hidden" id="historyformsesstoken" name="session_token" />
         </form>
@@ -31,9 +31,9 @@ echoheader();
               <a href="#" id="navBtnUntrust" class="btn btn-danger navbar-btn" title="Delete all cookies"><i class="glyphicon glyphicon-fire"></i> <strong class="hidden-xs">Untrust</strong></a>
           </div>
           <!-- Required bootstrap placeholder for the collapsed menu -->
-          <button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle" style="margin-left:10px"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+          <button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
         </div>
-        <div id="navbar" class="collapse navbar-collapse navbar-left" style="min-width:100px;">
+        <div id="navbar" class="collapse navbar-collapse navbar-left">
           <ul class="nav navbar-nav" id="nav_links">
             <li id="nav-add"><a href="" data-toggle="modal" data-target="#add">Add Entry</a></li>
             <li id="nav-pin"><a href="" data-toggle="modal" data-target="#pin">Set PIN</a></li>
@@ -61,19 +61,19 @@ echoheader();
         </div>
         <div class="col-md-4">
             <div class="pull-right-sm" id="rightHandBox">
-                <div id="tagCloud" style="display:none;">
-                    <p class="lead" style="margin-bottom:0">Tag-Overview<a id="orderTags" name="enable grouping" class="small" style="padding-left:10px"><span class="glyphicon glyphicon-sort-by-attributes"></span></a><a id="orderTagsDisable" name="disable grouping" class="small" style="padding-left:10px; display:none;"><span class="glyphicon glyphicon-remove"></span></a></p>
-                    <p class="visible-xs small" style ="margin-bottom:0;">
+                <div id="tagCloud" class="hidden">
+                    <p class="lead">Tag-Overview<a id="orderTags" name="enable grouping" class="small"><span class="glyphicon glyphicon-sort-by-attributes"></span></a><a id="orderTagsDisable" name="disable grouping" class="small hidden"><span class="glyphicon glyphicon-remove"></span></a></p>
+                    <p class="visible-xs small">
                         <a id="tagsShow"><span class="tagsShow">show</span><span class="tagsShow hidden">hide</span> tags</a>
                     </p>
-                    <span class="hidden-xs" id="tags"></span><p class="small" style="display:none;" id="resetFilter"><a id="tagsFilter">reset filter</a></p>
+                    <span class="hidden-xs" id="tags"></span><p class="small hidden" id="resetFilter"><a id="tagsFilter">reset filter</a></p>
                 </div>
             </div>
         </div>
     </div>
-    <div id="message" class="alert" style="display:none;"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><span id="messageText"></span></div>
+    <div id="message" class="alert hidden"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><span id="messageText"></span></div>
     <div id="waitsign">PLEASE WAIT WHILE WE ARE DECRYPTING YOUR PASSWORD...</div>
-    <div id="pwdtable" style="display:none">
+    <div id="pwdtable" class="hidden">
     <br />
     <table class="table table-striped table-bordered" id="pwdlist">
     <thead>
@@ -119,10 +119,10 @@ echoheader();
             </div>
             <div class="modal-body">
             <form>
-                <p id="fileincludeckbp" style="display:none"><input type="checkbox" id="fileincludeckb" />Include Files<br /></p>
+                <p id="fileincludeckbp" class="hidden"><input type="checkbox" id="fileincludeckb" />Include Files<br /></p>
                 <p>You will need your CURRENT login password to unlock the backup file even if you change login password later. Write your CURRENT login password down or remember to generate a new backup file after each time you change the login password.</p>
-                <p style="color:red">Generating backup file is time consuming...</p>
-                <div class="progress"><div class="progress-bar" role="progressbar"  id="backuppwdpb" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%"></div>
+                <p class="textred">Generating backup file is time consuming...</p>
+                <div class="progress"><div class="progress-bar" role="progressbar"  id="backuppwdpb" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
             </form>
             </div>
             <div class="modal-footer">
@@ -143,9 +143,9 @@ echoheader();
             <div class="modal-body">
             <form>
             <p>Please edit the fields string according to the default one shown below. It should be JSON format. Usually, you don't need to edit this.</p>
-            <p>A field entry should be like: <span style="font-weight:bold">"<span style="color:red">label</span>":{"colname":"<span style="color:red">screen name</span>","hint":"<span style="color:red">input hint</span>","cls":" <span style="color:red">style</span>"}</span>, red parts are for you to customize. Entries should be seperated with comma, and the final fields string should look like <span style="font-weight:bold">{entry1, entry2, entry3}</span>. <span style="font-weight:bold">{}</span> fields string means you don't want any additional fields</p>
-            <p><span style="color:red">label</span> is a system label for this entry, which should be UNIQUE and won't show up in user screen. <span style="color:red">screen name</span> is the name for this entry that the user see on their screen, need not to be unique. <span style="color:red">input hint</span> is the hint string that will show up when you try to edit values for this field. <span style="color:red">style</span> is the display style for this field. If <span style="color:red">style</span> string is empty, this field will always show up. If <span style="color:red">style = hidden</span>, this field will not show up in main table. You can only access it by clicking [view details]. If <span style="color:red">style = hidden-xs</span>, this field will not show up in main table on small screens such as smart phone screens, but will show up on computer screens. NO OTHER VALUES ALLOWED for <span style="color:red">style</span>. There should always be a space before <span style="color:red">style</span> string, don't delete it.</p>
-            <textarea class="form-control" id="fieldsz" style="height:100px"></textarea>
+            <p>A field entry should be like: <span class="textbold">"<span class="textred">label</span>":{"colname":"<span class="textred">screen name</span>","hint":"<span class="textred">input hint</span>","cls":" <span class="textred">style</span>"}</span>, red parts are for you to customize. Entries should be seperated with comma, and the final fields string should look like <span class="textbold">{entry1, entry2, entry3}</span>. <span class="textbold">{}</span> fields string means you don't want any additional fields</p>
+            <p><span class="textred">label</span> is a system label for this entry, which should be UNIQUE and won't show up in user screen. <span class="textred">screen name</span> is the name for this entry that the user see on their screen, need not to be unique. <span class="textred">input hint</span> is the hint string that will show up when you try to edit values for this field. <span class="textred">style</span> is the display style for this field. If <span class="textred">style</span> string is empty, this field will always show up. If <span class="textred">style = hidden</span>, this field will not show up in main table. You can only access it by clicking [view details]. If <span class="textred">style = hidden-xs</span>, this field will not show up in main table on small screens such as smart phone screens, but will show up on computer screens. NO OTHER VALUES ALLOWED for <span class="textred">style</span>. There should always be a space before <span class="textred">style</span> string, don't delete it.</p>
+            <textarea class="form-control" id="fieldsz"></textarea>
             </form>
             </div>
             <div class="modal-footer">
@@ -164,7 +164,7 @@ echoheader();
             </div>
             <div class="modal-body">
             <form>
-            <div class="form-control" id="details" style="height:230px; background:#efefef; overflow:auto" ></div>
+            <div class="form-control" id="details"></div>
             </form>
             </div>
             <div class="modal-footer">
@@ -218,8 +218,8 @@ echoheader();
                     <div class="form-group">
                         <label for="pinxx" class="control-label">PIN:</label>
                         <input id="pinxx" autocomplete="off" class="form-control" type="password" />
-                        <label class="small" style="display:block; clear:both; color:red">Only set PIN in your trusted devices!</label>
-                        <label class="small" style="display:block; clear:both;">PIN can be set on your trusted devices to give you convenience while login. If you set PIN, you can use PIN instead of username and password to login next time. PIN is safe, you only have 3 chances to input a PIN before it's disabled automatically.</label>
+                        <label class="small textred blocklabel">Only set PIN in your trusted devices!</label>
+                        <label class="small blocklabel">PIN can be set on your trusted devices to give you convenience while login. If you set PIN, you can use PIN instead of username and password to login next time. PIN is safe, you only have 3 chances to input a PIN before it's disabled automatically.</label>
                     </div>
             </div>
             <div class="modal-footer">
@@ -242,7 +242,7 @@ echoheader();
                     <div class="form-group">
                         <label for="importc" class="control-label">You can import passwords from CSV file or raw backup file. Select a .csv file or .raw file to start.</label>
                         <input type="file" id="importc" accept=".csv,.raw" />
-                        <label class="small" style="display:block; clear:both;">CSV file must contain a header line with columns including "name" and "password" - order is not important. You may edit your CSV with your password in Office so that the account field has a header called 'name' and the password field has a header called 'password'. Other columns will only be imported if they have the same header name as one of your additional fields. Note your CSV file must be in UTF-8 encoding. If not, open your CSV in some plaintext editor and change the encoding to UTF-8 before importing.</label>
+                        <label class="small blocklabel">CSV file must contain a header line with columns including "name" and "password" - order is not important. You may edit your CSV with your password in Office so that the account field has a header called 'name' and the password field has a header called 'password'. Other columns will only be imported if they have the same header name as one of your additional fields. Note your CSV file must be in UTF-8 encoding. If not, open your CSV in some plaintext editor and change the encoding to UTF-8 before importing.</label>
                     </div>
                 </form>
             </div>
@@ -264,9 +264,9 @@ echoheader();
             <div class="modal-body">
                 <form>
                     <div class="form-group">
-                        <label for="uploadf" class="control-label">You can upload one attachment for <span id="uploadfitemlab1" style="font-weight: bold;"></span>.</label>
+                        <label for="uploadf" class="control-label">You can upload one attachment for <span id="uploadfitemlab1" class="textbold"></span>.</label>
                         <input type="file" id="uploadf" />
-                        <label class="small" style="display:block; clear:both; color:red">Warning: If you already have an attachment for <span id="uploadfitemlab2" style="font-weight: bold;"></span>, the old attachment will be overwritten. Maximal file size allowed is 3MB.</label>
+                        <label class="small textred blocklabel">Warning: If you already have an attachment for <span id="uploadfitemlab2" class="textbold"></span>, the old attachment will be overwritten. Maximal file size allowed is 3MB.</label>
                     </div>
                 </form>
             </div>
