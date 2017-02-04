7327ad160119bcea6e7252c19a0531edc035fbd9
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-02-04 12:37:42 +0100

    downloadfile.php based on function/ajax.php

--- src/rest/downloadfile.php
+++ src/rest/downloadfile.php
@@ -1,19 +1,22 @@
 <?php
-function error($msg) {
-    echo json_encode(array("status" => "error", "message" => $msg));
-    die();
-}
+require_once(dirname(__FILE__).'/../function/ajax.php');
 require_once(dirname(__FILE__)."/../function/sqllink.php");
 $link=sqllink();
-if(!checksession($link)) error("AUTHENTICATION ERROR, PLEASE RELOGIN");
+if(!checksession($link)) {
+    error("AUTHENTICATION ERROR, PLEASE RELOGIN");
+}
 $id=$_SESSION['userid'];
-if(!isset($_POST['id'])||(int)$_POST['id']<1) error("Parameter Error");
+if(!isset($_POST['id'])||(int)$_POST['id']<1) {
+    error("Parameter Error");
+}
 $index=(int)$_POST['id'];
 
 $sql = "SELECT `key`,`files` FROM `files` WHERE `userid`= ? and `index`=?";
 $res = sqlexec($sql,array($id,$index),$link);
 $record = $res->fetch(PDO::FETCH_ASSOC);
-if($record==False) error("No File Can Be Downloaded");
+if($record==False) {
+    error("No File Can Be Downloaded");
+}
 $result=array();
 $result['status']='success';
 $result['key']=$record['key'];
