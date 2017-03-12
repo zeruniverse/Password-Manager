56bb36f9d9a8768874d8f5020ad55d2b0dbdcbee
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-03-12 18:05:46 +0100

    correct update script

--- removeEncryption.php
+++ removeEncryption.php
@@ -1,27 +1,5 @@
 <?php
-function encrypt($data, $key)
-{
-	$key	=	md5($key);
-    $x		=	0;
-	$char='';
-	$str='';
-    $len	=	strlen($data);
-    $l		=	strlen($key);
-    for ($i = 0; $i < $len; $i++)
-    {
-        if ($x == $l) 
-        {
-        	$x = 0;
-        }
-        $char .= $key{$x};
-        $x++;
-    }
-    for ($i = 0; $i < $len; $i++)
-    {
-        $str .= chr(ord($data{$i}) + (ord($char{$i})) % 256);
-    }
-    return base64_encode($str);
-}
+require_once("/src/function/sqllink.php");
 function decrypt($data, $key)
 {
 	$key = md5($key);
@@ -53,4 +31,44 @@ function decrypt($data, $key)
     }
     return $str;
 }
+$link = sqllink();
+if(!checksession($link)) {
+    die('0');
+}
+if(!$link->beginTransaction()) {
+    die('0');
+}
+$sql = "SELECT * FROM `password`";
+$res = sqlexec($sql,[],$link);
+while ($i = $res->fetch(PDO::FETCH_ASSOC)){
+    $update = "UPDATE `password` SET pubkey = "", pwd = ? WHERE userid=?, index=?";
+    $newpwd=$i['pwd'];
+    if ($i['pubkey']!= "")
+        $newpwd = decrypt($i['pwd'],$i['pubkey']);
+    $ures = sqlexec($update,array($newpwd,$i['userid'],$i['index']), $link);
+    if ($res == NULL) {
+        $link->rollBack();
+        die(1);
+    }
+}
+$sql = "AÖTER TABLE `password` DROP `pubkey`";
+$res = sqlexec($sql,[],$link);
+if ($res == NULL) {
+        $link->rollBack();
+        die(2);
+}
+
+$sql = "SELECT * FROM `pwdusrrecord`";
+$res = sqlexec($sql,[],$link);
+while ($i = $res->fetch(PDO::FETCH_ASSOC)){
+    $update = "UPDATE `pwdusrrecord` SET password = ? WHERE id=?";
+    $newpwd=$i['pwd'];
+    $newpwd = decrypt($i['pwd'],$GLOBAL_SALT_3);
+    $ures = sqlexec($update,array($newpwd,$i['id']), $link);
+    if ($res == NULL) {
+        $link->rollBack();
+        die(3);
+    }
+}
+$link->commit();
 ?>
