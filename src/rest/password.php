6a1860d75e3f2d59c224c42b8c12cef13038a2fb
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-04-02 12:38:10 +0200

    gather login information

--- src/rest/password.php
+++ src/rest/password.php
@@ -19,6 +19,7 @@ $result["fields_allow_change"] = $CUSTOMIZE_FIELDS;
 $result["server_timeout"] = $SERVER_TIMEOUT;
 $result["file_enabled"] = $FILE_ENABLED?1:0;
 
+// Select Accounts
 $sql = "SELECT * FROM `password` WHERE `userid` = ?";
 $res = sqlexec($sql,array($id),$link);
 $accounts = array();
@@ -27,6 +28,7 @@ while ($i = $res->fetch(PDO::FETCH_ASSOC)){
 }
 $result["accounts"] = $accounts;
 
+// Select Files
 $sql = "SELECT `index`,`fname`,`key` FROM `files` WHERE `userid` = ?";
 $res = sqlexec($sql,array($id),$link);
 $fdata = array();
@@ -34,5 +36,18 @@ while ($i = $res->fetch(PDO::FETCH_ASSOC)){
     $fdata[] = array( "index" => $i['index'], "fname" => $i['fname'], "fkey" => $i['key']);
 }
 $result["fdata"] = $fdata;
+
+// Select last login
+$sql = "SELECT `id`, UNIX_TIMESTAMP(`time`) AS `time` FROM `history` WHERE `userid` = ? AND `outcome` = 1 ORDER BY `id` DESC LIMIT 1 OFFSET 1";
+$res = sqlexec($sql, array($id),$link);
+$data = $res->fetch(PDO::FETCH_ASSOC);
+$loginID = $data["id"];
+$result["loginInformation"] = array( "lastLogin" => $data["time"] );
+
+// Select failed login attempts
+$sql = "SELECT COUNT(*) AS `failedLogins` FROM `history` WHERE `userid` = ? AND `outcome` = 0 AND `id` > ?";
+$res = sqlexec($sql, array($id, $loginID),$link);
+$result["loginInformation"]["failedCount"] = $res->fetch(PDO::FETCH_ASSOC)["failedLogins"];
+
 echo json_encode($result);
 ?>
