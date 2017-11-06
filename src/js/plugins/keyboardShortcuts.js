4a4dc1b1dd5f64d00003f0d6acba5b067d3416d4
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-11-06 19:59:08 +0100

    spell proofing of Keyboard Shortcut help

--- src/js/plugins/keyboardShortcuts.js
+++ src/js/plugins/keyboardShortcuts.js
@@ -2,9 +2,9 @@
 
 // define Shortcuts (keycode: {action: action to do (modal, click, focus), element: element to focus, description: help text})
 var keyboardShortcuts = {
-    '97': {"action":"modal", "element":"#add", "description": "show add entry dialog"},
-    '115': {"action":"focus", "element":"#pwdlist_filter > label > input", "description": "focus search input"},
-    '63': {"action":"modal", "element":"#shortcuts", "description": "show/hide keyboard shortcut help"},
+    '97': {"action":"modal", "element":"#add", "description": "Show add entry dialog"},
+    '115': {"action":"focus", "element":"#pwdlist_filter > label > input", "description": "Focus search input"},
+    '63': {"action":"modal", "element":"#shortcuts", "description": "Show keyboard shortcut help"},
 }
 
 var actions = {
@@ -38,7 +38,7 @@ registerPlugin("layoutReady", function(data){
                     .append($('<div class="modal-dialog"></div>')
                         .append($('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'))
                         .append($('<div class="modal-content"></div')
-                            .append($('<div class="modal-header"><h4>Add a new account</h4></div>'))
+                            .append($('<div class="modal-header"><h4>Keyboard Shortcuts</h4></div>'))
                             .append($('<div class="modal-body"></div>')
                                 .append(list))
                             .append($('<div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button> <button type="button" class="btn btn-primary" id="newbtn">Add</button> </div>'))
