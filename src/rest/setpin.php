cb7ae610d9dfe1b24249d306a8a20e62016e0544
Benjamin Häublein <BenjaminHae@users.noreply.github.com>
2017-11-29 20:04:03 +0100

    Secure random (#182)
    use cryptographically secure random number for pin
    random login stamp is now really random

--- src/rest/setpin.php
+++ src/rest/setpin.php
@@ -1,17 +1,9 @@
 <?php
 
+// only even lengths are allowed
 function random_str($length)
 {
-    $arr = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z'));
-
-    $str = '';
-    $arr_len = count($arr);
-    for ($i = 0; $i < $length; $i++) {
-        $rand = mt_rand(0, $arr_len - 1);
-        $str .= $arr[$rand];
-    }
-
-    return $str;
+    return bin2hex(openssl_random_pseudo_bytes($length / 2));
 }
 require_once dirname(__FILE__).'/../function/sqllink.php';
 $link = sqllink();
@@ -30,7 +22,7 @@ if ($username != $usr) {
 $sql = 'DELETE FROM `pin` WHERE `userid` = ? AND `device` = ?';
 $res = sqlexec($sql, [$id, $device], $link);
 
-$pinpk = random_str(29);
+$pinpk = random_str(30);
 
 $sql = 'INSERT INTO `pin` (`userid`,`device`,`pinsig`,`pinpk`,`ua`) VALUES (?,?,?,?,?)';
 $res = sqlexec($sql, [$id, $device, $sig, $pinpk, $_SERVER['HTTP_USER_AGENT']], $link);
