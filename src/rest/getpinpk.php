5cc9024c0f7bf261600e36f7417564b1f07841ce
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-12-03 12:04:35 +0100

    getpinpk uses JSON result

--- src/rest/getpinpk.php
+++ src/rest/getpinpk.php
@@ -8,18 +8,18 @@ session_start();
 require_once dirname(__FILE__).'/../function/sqllink.php';
 $link = sqllink();
 if (!$link) {
-    die(0);
+    error("Database connection failed");
 }
 $user = $_POST['user'];
 $device = $_POST['device'];
 if ($user == '' || $device == '') {
-    die('0');
+    error("No PIN available");
 }
 $sql = 'SELECT `id` FROM `pwdusrrecord` WHERE `username`= ?';
 $res = sqlexec($sql, [$user], $link);
 $record = $res->fetch(PDO::FETCH_ASSOC);
 if ($record == false) {
-    die('0');
+    error("No PIN available");
 }
 $id = $record['id'];
 $sql = 'DELETE FROM `pin` WHERE `errortimes` >= 3 OR UNIX_TIMESTAMP( NOW( ) ) - UNIX_TIMESTAMP(`createtime`) > ?';
@@ -28,14 +28,16 @@ $sql = 'SELECT `pinsig`,`pinpk` FROM `pin` WHERE `userid`= ? AND `device`=?';
 $res = sqlexec($sql, [$id, $device], $link);
 $record = $res->fetch(PDO::FETCH_ASSOC);
 if ($record == false) {
-    die('0');
+    error("No PIN available");
 }
 $sig = $record['pinsig'];
 if (strcmp(hash('sha512', (string) $sig.(string) $_SESSION['random_login_stamp']), (string) $_POST['sig']) == 0) {
     $sql = 'UPDATE `pin` SET `errortimes`=0 WHERE `userid`= ? AND `device`=?';
     $res = sqlexec($sql, [$id, $device], $link);
-    die($record['pinpk']);
+
+    echo json_encode(['status' => "success", "pinpk" => $record['pinpk']]);
+    die();
 }
 $sql = 'UPDATE `pin` SET `errortimes`=`errortimes`+1 WHERE `userid`= ? AND `device`=?';
 $res = sqlexec($sql, [$id, $device], $link);
-die('1');
+error("To many tries. PIN deleted.");
