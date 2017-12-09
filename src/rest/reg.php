28b29618790f3543f997681154120853a70599fd
Benjamin Häublein <benjaminhaeublein@gmail.com>
2017-12-09 20:13:25 +0100

    reg.php uses json response

--- src/rest/reg.php
+++ src/rest/reg.php
@@ -1,44 +1,43 @@
 <?php
-
+require_once dirname(__FILE__).'/../function/ajax.php';
+require_once dirname(__FILE__).'/../function/sqllink.php';
+if ($ALLOW_SIGN_UP === false) {
+    http_response_code(405);
+    error('Signup is not allowed.');
+}
 $pw = $_POST['pwd'];
 $usr = $_POST['user'];
 $email = $_POST['email'];
 if ($pw == '' || $usr == '' || $email == '') {
-    die('7');
+    error("Not all required parameters have been entered.");
 }
 // check length of password hash for pbkdf2
 if (strlen($pw) > 130) {
-    die('2');
+    error("password too long");
 }
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die('5');
+    error("Invalid E-mail address.");
 }
-require_once dirname(__FILE__).'/../function/sqllink.php';
-if ($ALLOW_SIGN_UP === false) {
-    http_response_code(405);
-    die('Method not allowed');
-}
-
 $link = sqllink();
 if (!$link) {
-    die('6');
+    error("There're some errors, please retry");
 }
 if (!$link->beginTransaction()) {
-    die('4');
+    error("There're some errors, please retry");
 }
 $sql = 'SELECT COUNT(*) FROM `pwdusrrecord` WHERE `username` = ?';
 $res = sqlexec($sql, [$usr], $link);
 $num = $res->fetch(PDO::FETCH_NUM);
 if ($num[0] != 0) {
     $link->commit();
-    die('0');
+    error('User name already occupied, please choose another user name.');
 }
 $sql = 'SELECT COUNT(*) FROM `pwdusrrecord` WHERE `email` = ?';
 $res = sqlexec($sql, [$email], $link);
 $num = $res->fetch(PDO::FETCH_NUM);
 if ($num[0] != 0) {
     $link->commit();
-    die('1');
+    error('This E-mail has already been used.');
 }
 $salt = openssl_random_pseudo_bytes(32);
 $pw = hash_pbkdf2('sha256', $pw, $salt, $PBKDF2_ITERATIONS);
@@ -49,7 +48,7 @@ $sql = 'INSERT INTO `pwdusrrecord` VALUES (?,?,?,?,?,?)';
 $rett = sqlexec($sql, [$maxnum + 1, $usr, $pw, $salt, $DEFAULT_FIELDS, $email], $link);
 if (!$rett) {
     $link->rollBack();
-    die('8');
+    error("There're some errors, please retry");
 }
 $link->commit();
-die('9');
+echo json_encode(['status' => "success"]);
