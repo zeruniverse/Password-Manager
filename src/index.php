72668fb415021c122e24ad4f123153ad36fe9cec
Pofilo <Pofilo@users.noreply.github.com>
2018-02-07 17:10:19 +0100

    HTML improvements (#191)
    * fix some html issues
    
    * improve html using https://github.com/validator/validator
    
    * fix HTML tags order
    
    * revert <hx> changes

--- src/index.php
+++ src/index.php
@@ -2,12 +2,12 @@
 require_once 'function/basic.php';
 echoheader();
 ?>
-<script type="text/javascript" src="js/crypto/aes.js"></script>
-<script type="text/javascript" src="js/crypto/sha512.js"></script>
-<script type="text/javascript" src="js/crypto/pbkdf2.js"></script>
-<script type="text/javascript" src="js/crypto/password.js"></script>
-<script type="text/javascript" src="js/setlocalstorage.js"></script>
-<script type="text/javascript" src="js/index.js"></script>
+<script src="js/crypto/aes.js"></script>
+<script src="js/crypto/sha512.js"></script>
+<script src="js/crypto/pbkdf2.js"></script>
+<script src="js/crypto/password.js"></script>
+<script src="js/setlocalstorage.js"></script>
+<script src="js/index.js"></script>
     <div class="container theme-showcase">
         <div class="page-header">
             <h1>Password Manager</h1>
@@ -40,20 +40,21 @@ echoheader();
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4>Use PIN to login</h4>
                 </div>
-                <div class="modal-body">
-                    <form id="pinloginform" method="post">
+                <form id="pinloginform" method="post">
+                    <div class="modal-body">
                         <div class="form-group">
                             <label for="pin" class="control-label">PIN:</label>
                             <input id="pin" autocomplete="off" class="form-control" type="password" />
                             <label class="small blocklabel">You see this window because you or someone set an PIN in this device and choose it as default login method. To switch account or disable PIN, please press the red button below. To use username/password to login only this time, close this window by pressing the 'X' at top-right corner.</label>
                             <label class="small textred blocklabel">Closing this window only let you use username/password to login this time. PIN will still be chosen as default method in future. Press red button below if you want to disable current PIN.</label>
                         </div>
-                </div>
-                <div class="modal-footer">
-                    <p class="collapse" id="pinerrorhint">PIN ERROR, try again.</p>
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Delete PIN</button>
-                    <input type="submit" class="btn btn-primary" id="pinlogin" value="Login" /></form>
-                </div>
+                    </div>
+                    <div class="modal-footer">
+                        <p class="collapse" id="pinerrorhint">PIN ERROR, try again.</p>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Delete PIN</button>
+                        <input type="submit" class="btn btn-primary" id="pinlogin" value="Login" />
+                    </div>
+                </form>
             </div>
         </div>
     </div>
