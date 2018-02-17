685d45ff86fd16d022ab6f4c9e42f8c9c95e1237
Benjamin Häublein <benjaminhaeublein@gmail.com>
2018-02-17 19:34:35 +0100

    added logout.php as REST

---
+++ src/rest/logout.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'function/sqllink.php';
+require_once dirname(__FILE__).'/../function/ajax.php';
+session_start();
+if (isset($_SESSION['loginok'])) {
+    unset($_SESSION['loginok']);
+}
+if (isset($_SESSION['user'])) {
+    unset($_SESSION['user']);
+}
+if (isset($_SESSION['pwd'])) {
+    unset($_SESSION['pwd']);
+}
+if (isset($_SESSION['fields'])) {
+    unset($_SESSION['fields']);
+}
+if (isset($_SESSION['userid'])) {
+    unset($_SESSION['userid']);
+}
+if (isset($_SESSION['random_login_stamp'])) {
+    unset($_SESSION['random_login_stamp']);
+}
+session_regenerate_id(true); //as suggested by owasp, change sessionId when changing context
+session_destroy();
+ajaxSuccess();
