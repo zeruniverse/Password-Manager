a333363abc4e4c412610399082806ff9e8eecfac
Benjamin Häublein <benjaminhaeublein@gmail.com>
2018-02-17 20:01:56 +0100

    moved cookie functions to commonFunctions

--- src/js/common/commonFunctions.js
+++ src/js/common/commonFunctions.js
@@ -73,3 +73,20 @@ function base64toBlob(base64Data, contentType) {
     }		
     return new Blob(byteArrays, { type: contentType });		
 }
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "-1";
+}
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/ ";
+}
