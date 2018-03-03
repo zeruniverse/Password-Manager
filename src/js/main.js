ffd46a57b95572d38159595ae11cce97e1264ac7
Benjamin Häublein <benjaminhaeublein@gmail.com>
2018-03-03 11:26:16 +0100

    fix generation of passphrase

--- src/js/main.js
+++ src/js/main.js
@@ -575,7 +575,7 @@ $(document).ready(function(){
         $(".popover").popover('hide');
     });
     $('#editPasswordInput').on('click', function() {
-        $('#edititeminputpw').val(getpwd(default_letter_used, default_length));
+        $('#edititeminputpw').val(backend.encryptionWrapper.generatePassphrase(default_length));
         $('#editAccountShowPassword').removeClass('collapse');
         $('#editAccountShowPassword').popover({
             'placement':'bottom',
