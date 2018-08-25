5e16b6a81c42323261a918d9b8bb7d588821fd1a
Benjamin Häublein <benjaminhaeublein@gmail.com>
2018-08-25 15:44:15 +0200

    fix syntax error

--- src/js/plugins/passwordHistory.js
+++ src/js/plugins/passwordHistory.js
@@ -26,7 +26,7 @@ registerPlugin("updateAccountPreSend", function(data){
     if (data["newPassword"] != "") {
         var account = data["account"];
         // create passwordHistory array for account if it doesn't exist
-        if (!"_passwordHistory" in account.other) {
+        if (!("_passwordHistory" in account.other)) {
             account.setOther("_passwordHistory", []);
         }
         var newHistoryEntry = {"password": data["oldData"]["password"]};
