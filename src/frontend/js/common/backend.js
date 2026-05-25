835764e543cc0142895a41d006cecaff605e8622
Zeyu Zhao <zzy8200@gmail.com>
2026-05-25 22:39:28 +0800

    Update PASSWORD_MANAGER_VERSION to 13.01

--- src/frontend/js/common/backend.js
+++ src/frontend/js/common/backend.js
@@ -1,6 +1,6 @@
 // helpers for javascript mixins-pattern
 let mix = (superclass) => new MixinBuilder(superclass);
-var PASSWORD_MANAGER_VERSION = '13.00';
+var PASSWORD_MANAGER_VERSION = '13.01';
 class MixinBuilder {
     constructor(superclass) {
         this.superclass = superclass;
